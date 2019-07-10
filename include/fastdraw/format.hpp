d2436fb717c2d19dc4b5c5566eb2a94aaa7688f8
Felipe Magno de Almeida <felipe@expertisesolutions.com.br>
2019-07-10 15:23:26 -0300

    Added png image loader

---
+++ include/fastdraw/format.hpp
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2019 Felipe Magno de Almeida.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// See http://www.boost.org/libs/foreach for documentation
+//
+
+#ifndef FASTDRAW_FORMAT_HPP
+#define FASTDRAW_FORMAT_HPP
+
+namespace fastdraw {
+
+enum class buffer_format
+{
+ unknown = 0,
+ r8g8b8,
+ r8g8b8a8,
+ gray8
+};
+  
+}
+
+#endif
+
---
+++ include/fastdraw/image_loader/png.hpp
@@ -0,0 +1,163 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2019 Felipe Magno de Almeida.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// See http://www.boost.org/libs/foreach for documentation
+//
+
+#ifndef FASTDRAW_IMAGE_LOADERS_PNG_HPP
+#define FASTDRAW_IMAGE_LOADERS_PNG_HPP
+
+#include <fastdraw/format.hpp>
+
+#include <filesystem>
+
+namespace fastdraw { namespace image_loader {
+
+struct png
+{
+  png_structp png_ptr;
+  png_infop info_ptr;
+
+  struct file_deleter
+  {
+    void operator()(FILE* fp) const { if (fp) fclose(fp); }
+  };
+  
+  std::unique_ptr<FILE, file_deleter> fp;
+  
+  png (std::filesystem::path path)
+  {
+    fp.reset (std::fopen (path.c_str(), "rb"));
+    if (!fp)
+      throw -1;
+
+    png_byte header[8];
+
+    fread (header, 1, 8, fp.get());
+    if ( ::png_sig_cmp (header, 0, 8))
+      throw -1;
+
+    png_ptr = ::png_create_read_struct (PNG_LIBPNG_VER_STRING, this, nullptr, nullptr);
+    if (!png_ptr)
+      throw -1;
+
+    info_ptr = ::png_create_info_struct (png_ptr);
+    if (!info_ptr)
+      throw -1;
+
+    if (setjmp ( png_jmpbuf (png_ptr)))
+      throw -1;
+
+    ::png_init_io (png_ptr, fp.get());
+    ::png_set_sig_bytes (png_ptr, 8);
+
+    ::png_read_info (png_ptr, info_ptr);
+
+    png_uint_32 width, height;
+    int bit_depth, color_type;
+    png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth,
+                 &color_type, NULL, NULL, NULL);    
+
+    if (color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_expand(png_ptr);
+    if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+        png_set_expand(png_ptr);
+    if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
+        png_set_expand(png_ptr);
+
+    if (bit_depth == 16)
+      png_set_strip_16(png_ptr);
+    if (color_type == PNG_COLOR_TYPE_GRAY ||
+        color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+      png_set_gray_to_rgb(png_ptr);
+
+    png_read_update_info(png_ptr, info_ptr);
+    
+    /*
+                     PNG_COLOR_TYPE_GRAY
+                        (bit depths 1, 2, 4, 8, 16)
+                     PNG_COLOR_TYPE_GRAY_ALPHA
+                        (bit depths 8, 16)
+                     PNG_COLOR_TYPE_PALETTE
+                        (bit depths 1, 2, 4, 8)
+                     PNG_COLOR_TYPE_RGB
+                        (bit_depths 8, 16)
+                     PNG_COLOR_TYPE_RGB_ALPHA
+                        (bit_depths 8, 16)
+
+                     PNG_COLOR_MASK_PALETTE
+                     PNG_COLOR_MASK_COLOR
+                     PNG_COLOR_MASK_ALPHA
+    */
+  }
+
+  ~png()
+  {
+    if (png_ptr)
+      png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+  }
+
+  png(png&& other)
+    : png_ptr (other.png_ptr), info_ptr (other.info_ptr)
+  {
+    png_ptr = nullptr;
+    info_ptr = nullptr;
+  }
+
+  int32_t width () const
+  {
+    return png_get_image_width (png_ptr, info_ptr);
+  }
+
+  int32_t height () const
+  {
+    return png_get_image_height (png_ptr, info_ptr);
+  }
+
+  fastdraw::buffer_format format () const
+  {
+    auto bit_depth = png_get_bit_depth (png_ptr, info_ptr);
+    auto color_type = png_get_color_type (png_ptr, info_ptr);
+    switch (color_type)
+    {
+    case PNG_COLOR_TYPE_RGB:
+      return buffer_format::r8g8b8;
+    case PNG_COLOR_TYPE_RGBA:
+      return buffer_format::r8g8b8a8;
+    default:
+      return buffer_format::unknown;
+    };
+  }
+
+  std::uint32_t stride () const
+  {
+    return png_get_rowbytes (png_ptr, info_ptr);
+  }
+
+  void write_to (char* buffer, std::size_t size) const
+  {
+    png_byte** row_pointers = new png_byte*[height()];
+    for (int32_t i = 0; i != height(); ++i)
+    {
+      if (png_ptr)
+        row_pointers[i] = static_cast<png_byte*>(static_cast<void*>(buffer)) + i * stride();
+      else
+        row_pointers[i] = (i ? row_pointers[i - 1] : static_cast<png_byte*>(static_cast<void*>(buffer)));
+    }
+  }
+};
+
+struct png_loader
+{
+  png load (std::filesystem::path path) const
+  {
+    return {path};
+  }
+};
+    
+} }
+
+#endif
